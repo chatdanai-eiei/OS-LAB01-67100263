cf32eeb87e6e6c1157e9ea3bbaf7a6eef9ae6c0a
Ormsxp <chatdanainasen101@gmail.com>
2026-08-30 08:13:21 +0000

    Completed Lab 2 coding

--- stress_test.py
+++ stress_test.py
@@ -3,6 +3,10 @@
 # Student ID: [Enter your ID here]
 # ==========================================
 import time
+import math
 
 # TODO: Write the intensive computation loop here
 # Follow the instructions in the Lab manual.
+print("Starting CPU Stress Test... Press Ctrl+C to stop.")
+while True:
+    math.factorial(50000) # Intensive calculation
